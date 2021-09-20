b8a5783c06d395e08f678276d5619dde35e47dcf
James Hoofard <james.hoofard.iii@gmail.com>
2021-09-20 09:57:13 -0500

    search bar works on a basic level

--- controllers/api/searchRoutes.js
+++ controllers/api/searchRoutes.js
@@ -1,48 +1,60 @@
 const router = require('express').Router();
-const { Recipe } = require('../../models');
+const { Recipe, User } = require('../../models');
 // const withAuth = require('../../utils/auth');
 // const { search } = require('./userRoutes');
 const { Op } = require("sequelize");
 router.get(`/all/:search`, async (req, res) => {
   console.log("test");
-    try {
-      // Get all recipes and JOIN with user data
-      const recipeData = await Recipe.findAll({
-        where: {
-          name: {
-            [Op.like]: `%${req.params.search}%`
-          }
-        }
-           });
-      // Serialize data so the template can read it
-      const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
-      console.log("these are the matching recipes", recipes)
-      // Pass serialized data and session flag into template
-      res.render('searchresults', {recipes})
-    } catch (err) {
-      console.log(err)
-      res.status(500).json(err);
-    }
-  });
+  try {
+    // Get all recipes and JOIN with user data
+    const recipeData = await Recipe.findAll({
+      where: {
+        name: {
+          [Op.like]: `%${req.params.search}%`
+        },
+
+      }
+    });
+    // const users = userData.map((user) => user.get({ plain: true }));
+    // console.log(users)
+    // Serialize data so the template can read it
+    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+    // console.log("========")
+    // console.log(recipes[0].user_id)
+    // const userData = await User.findByPk(1)
+    // console.log(userData)
+    // const users = userData.get({ plain: true });
+    
+    
+    console.log("these are the matching recipes", recipes)
+    // Pass serialized data and session flag into template
+    res.render('searchresults', { recipes})
+    // res.render('searchresults', { recipes, user})
+
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
+});
 
 router.get(`/:search`, async (req, res) => {
   console.log("test");
-    try {
-      // Get all recipes and JOIN with user data
-      const recipeData = await Recipe.findAll({
-           where: {
-             name: req.params.name
-           },
-           });
-  
-      // Serialize data so the template can read it
-      const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
-  
-      // Pass serialized data and session flag into template
-      res.json(recipes)
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  try {
+    // Get all recipes and JOIN with user data
+    const recipeData = await Recipe.findAll({
+      where: {
+        name: req.params.name
+      },
+    });
 
-  module.exports=router
+    // Serialize data so the template can read it
+    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.json(recipes)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router
